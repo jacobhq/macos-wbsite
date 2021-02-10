0ae5c7e5f12ae9318ca8cb1759bf5e28edaf27f1
jacobhq <jacob.marshall.jem@gmail.com>
2021-02-10 13:00:57 +0000

    Make section two

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ new fullpage("#fullpage", {
     const img = section.querySelectorAll(".box img");
     const tl = new TimelineMax({ delay: 0.5 });
     tl.fromTo(img, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
-    if (destination.index === 1) {
-      const chairs = document.querySelectorAll(".chair");
+    if (destination.index === 2) {
+      const macs = document.querySelectorAll(".pc");
       const description = document.querySelector(".description");
-      tl.fromTo(chairs, 0.7, { x: "100%" }, { x: "-10%" })
+      tl.fromTo(macs, 0.7, { x: "100%" }, { x: "-10%" })
         .fromTo(
           description,
           0.5,
           { opacity: 0, y: "50" },
           { y: "0", opacity: 1 }
         )
-        .fromTo(chairs[0], 1, { opacity: 1 }, { opacity: 1 })
-        .fromTo(chairs[1], 1, { opacity: 0 }, { opacity: 1 })
-        .fromTo(chairs[2], 1, { opacity: 0 }, { opacity: 1 });
+        .fromTo(macs[0], 1, { opacity: 1 }, { opacity: 1 })
+        .fromTo(macs[1], 1, { opacity: 0 }, { opacity: 1 })
+        .fromTo(macs[2], 1, { opacity: 0 }, { opacity: 1 });
     }
   }
 });
