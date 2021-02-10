4eca98127e3e2af63249bad76bf131ee2929c6eb
jacobhq <jacob.marshall.jem@gmail.com>
2021-02-10 20:50:38 +0000

    Make mac turn on

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ new fullpage("#fullpage", {
         .fromTo(macs[1], 1, { opacity: 0 }, { opacity: 1 })
         .fromTo(macs[2], 1, { opacity: 0 }, { opacity: 1 });
     }
+    if (destination.index === 3) {
+        const states = document.querySelectorAll(".mm")
+        tl.fromTo(states[0], 0.7, { opacity: 0 }, { opacity: 1 })
+        .fromTo(states[1], 0.7, { opacity: 1 }, { opacity: 0 });
+    }
     if (destination.index === 4) {
       const card = document.querySelector(".s5 .bg");
       tl.fromTo(card, 0.7, { width: "85vw", height: "75vh", margin: "auto" }, { width: "100vw", height: "100vh" });
